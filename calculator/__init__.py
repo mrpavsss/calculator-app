b9b14047388fe93cf2bbe84e9dd2d3228072129b
pavly <pr253@njit.edu>
2022-03-03 11:01:41 -0500

    first one

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,19 +1,24 @@
 """ This is the Calculator Class"""
+from calculator.operations import Addition, Subtraction, Multiplication
 
 
 class Calculator:
     """ This is the default result property"""
-    result = 0
-
-    def add(self, value_1):
+    @staticmethod
+    def add(value_1, value_2):
         """ This is the add method"""
-        self.result = self.result + value_1
-        return self.result
+        # Call the static method add to return the sum and set it to the calculator result property
+        return Addition.add(value_1, value_2)
 
-    def subtract(self, value_1):
+    @staticmethod
+    def subtract(value_1, value_2):
         """ This is the subtract method"""
-        self.result = self.result - value_1
-        return self.result
+        return Subtraction.subtract(value_1, value_2)
+
+    @staticmethod
+    def multiply(self, value_1, value_2):
+        """ This is the subtract method"""
+        return Multiplication.multiply(value_1, value_2)
 
     def get_result(self):
         """ This is the get result method"""
