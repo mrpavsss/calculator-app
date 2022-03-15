8602bf00834339ba9c256379b46fdc2113c29d92
pavly <pr253@njit.edu>
2022-03-15 13:51:53 -0400

    maybe

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,8 +1,10 @@
 """ This is the Calculator Class"""
-from calculator.calculations import Addition, Subtraction, Multiplication
+from calculator.calculation import Addition, Subtraction, Multiplication
+
 
 class Calculator:
     """ This is the default result property"""
+
     @staticmethod
     def add(tuple_list):
         """ This is the add method"""
@@ -15,6 +17,7 @@ class Calculator:
         """ This is the subtract method"""
         calculation = Subtraction.create(tuple_list)
         return calculation.get_result()
+
     @staticmethod
     def multiply(tuple_list):
         """ This is the subtract method"""
